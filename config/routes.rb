75fcef1f460088b58edec93bfa9b49cffc6e1351
Julie Greene <julie_greene22@yahoo.com>
2020-02-18 16:22:52 -0700

    new note is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/new'
+  resources :notes 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
