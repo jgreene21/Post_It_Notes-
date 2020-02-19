02815ef8ed0ae8094219ea35a91f80372474f354
Julie Greene <julie_greene22@yahoo.com>
2020-02-18 23:11:41 -0700

    Final Post-it App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_notes/home'
+  get 'static_notes/about'
+  root 'static_notes#home'
+
+  get '/about', to: 'static_notes#about'
+
   resources :notes 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
